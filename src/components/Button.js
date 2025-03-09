af2dba8eed54ef457fdcdf7c98a3d53b204ea77f
Mikhil M <mighilmurali@gmail.com>
2025-03-10 00:01:15 +0530

    Added button list

---
+++ src/components/Button.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Button = ({name}) => {
+  return (
+    <div className='px-4 py-2 m-2 rounded-lg bg-gray-100'>{name}</div>
+  )
+}
+
+export default Button
--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import Button from './Button'
 
 const ButtonList = () => {
+
+    const buttons = ["All", "Gaming", "Songs", "Live", "Soccer", "Cricket", "Cooking"]
+
     return (
-        <div>ButtonList</div>
+        <div className='flex'>
+            {
+                buttons.map(button => <Button key={button} name={button} />)
+            }    
+        </div>
     )
 }
 
